caf12d2c5ae87944d9edfc32688d3a3c925a5abb
Jatin Sharma <sjatin.cse@gmail.com>
2023-03-24 16:39:47 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { store } from '../redux/store';
+import { Provider } from 'react-redux';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// create a client
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+  </QueryClientProvider>
+  )
 }
+
+export default MyApp
